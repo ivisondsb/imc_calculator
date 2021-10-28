baf4d44690be1b6dbdb6bde29fd4190e259ed5e1
Ivison <Ivisonk0@gmail.com>
2021-10-28 15:58:03 -0300

    add BMI calculation

--- app/src/main/java/com/ivisondsb/somatator/MainActivity.kt
+++ app/src/main/java/com/ivisondsb/somatator/MainActivity.kt
@@ -2,10 +2,23 @@ package com.ivisondsb.somatator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calc.setOnClickListener {
+            val df = DecimalFormat("0.00")
+            val peso = edtpeso.text.toString().toDouble()
+            val altura = alt.text.toString().toDouble()
+
+            val resultado = peso / (altura * altura)
+
+            result.text = "Seu IMC é: ${df.format(resultado)}"
+
+        }
     }
 }
