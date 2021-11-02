afc0dc8c576ea92201946029cbc5547fdc7826b5
Ivison <Ivisonk0@gmail.com>
2021-11-02 10:53:37 -0300

    show result according to imc table

--- app/src/main/java/com/ivisondsb/somatator/MainActivity.kt
+++ app/src/main/java/com/ivisondsb/somatator/MainActivity.kt
@@ -19,6 +19,21 @@ class MainActivity : AppCompatActivity() {
 
             result.text = "Seu IMC é: ${df.format(resultado)}"
 
+            if (resultado < 18.5) {
+                tab.text = "Abaixo do Peso Ideal"
+            }
+            if (resultado >= 18.5 && resultado <= 24.9) {
+                tab.text = "Peso Ideal"
+            }
+            if (resultado >= 25.0 && resultado <= 29.9) {
+                tab.text = "Acima do Peso Ideal"
+            }
+            if (resultado >= 30.0 && resultado <= 39.9) {
+                tab.text = "Obesidade"
+            }
+            if (resultado >= 40.0) {
+                tab.text = "Obesidade Grave"
+            }
         }
     }
 }
